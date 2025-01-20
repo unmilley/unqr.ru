a671b582d26338e96673baecbcddff55ae3d4e8c
unmilley <1337@milley.uno>
2025-01-21 03:45:25 +0500

    feat: `added cache for stats`

--- server/api/stats/index.get.ts
+++ server/api/stats/index.get.ts
@@ -1,33 +1,35 @@
 import { serverSupabaseClient } from '#supabase/server'
-import type { GithubStat, Stats } from '~/shared/types'
 
-export default defineEventHandler(async (event): Promise<Stats> => {
-  const { public: config } = useRuntimeConfig(event)
-  try {
-    const client = await serverSupabaseClient(event)
+export default defineCachedEventHandler(
+  async (event): Promise<Stats> => {
+    const { public: config } = useRuntimeConfig(event)
+    try {
+      const client = await serverSupabaseClient(event)
 
-    let { data } = await client.rpc('get_link_stats')
+      let { data } = await client.rpc('get_link_stats')
 
-    if (!data) data = [{ total_views: 0, total_users: 0, total_links: 0 }]
+      if (!data) data = [{ total_views: 0, total_users: 0, total_links: 0 }]
 
-    const github = await event.$fetch<GithubStat>(`https://api.github.com/repos/${config.gitRepo}`, {
-      parseResponse: (val) => {
-        const { stargazers_count, pushed_at } = JSON.parse(val)
-        return {
-          lastCommitAt: pushed_at as string,
-          stars: stargazers_count ?? 0,
-        }
-      },
-    })
+      const github = await event.$fetch<GithubStat>(`https://api.github.com/repos/${config.gitRepo}`, {
+        parseResponse: (val) => {
+          const { stargazers_count, pushed_at } = JSON.parse(val)
+          return {
+            lastCommitAt: pushed_at as string,
+            stars: stargazers_count ?? 0,
+          }
+        },
+      })
 
-    return {
-      totalViews: data[0].total_views,
-      totalUsers: data[0].total_users,
-      totalLinks: data[0].total_links,
-      ...github,
+      return {
+        totalViews: data[0].total_views,
+        totalUsers: data[0].total_users,
+        totalLinks: data[0].total_links,
+        ...github,
+      }
+    } catch (error: any) {
+      console.warn('api/stats/index.get: ', error.message)
+      return {} as Stats
     }
-  } catch (error: any) {
-    console.warn('api/stats/index.get: ', error.message)
-    return {} as Stats
-  }
-})
+  },
+  { maxAge: 60 * 60 * 2 }
+)
