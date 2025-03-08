0c65896a3670a3f8930a2563032b70ef40fece60
unmilley <1337@milley.uno>
2025-03-08 10:54:47 +0500

    feat: `utils from useQR to shared utils`

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,1 +1,3 @@
+import { checkHex, convertColor, fileToBase64, normalizeOptions } from '@/composables/useQR/utils'
 export * from './hex'
+export { checkHex, convertColor, fileToBase64, normalizeOptions }
